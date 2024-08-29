c962be81d45c8a13bf6fe2f299d19edf64060de5
JinT-Hwang <834007989@qq.com>
2024-08-30 03:58:51 +0800

    Initial commit

---
+++ Vox Populi 4.15.2 繁体汉化/(2) Vox Populi/Database Changes/Text/en_US/AI/DiplomacyResponseTextChanges.sql
@@ -0,0 +1,11 @@
+UPDATE Language_en_US
+SET Text = '我願意忽視你們最近在我的土地上進行的間諜活動,只要你們鄭重承諾不會再發生.'
+WHERE Tag = 'TXT_KEY_GENERIC_CAUGHT_YOUR_SPY_FRIENDLY_1';
+
+UPDATE Language_en_US
+SET Text = '你那可惡的間諜是怎麼突破我的防線的?這可能是我們最後說的話了.'
+WHERE Tag = 'TXT_KEY_GENERIC_CAUGHT_YOUR_SPY_HOSTILE_1';
+
+UPDATE Language_en_US
+SET Text = '我很高興你同意停止監視我的人.'
+WHERE Tag = 'TXT_KEY_GENERIC_HUMAN_CAUGHT_YOUR_SPY_GOOD_1';
